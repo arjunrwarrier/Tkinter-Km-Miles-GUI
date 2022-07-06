0006128e2816a8c30854bfe7b729714cbd34a882
Arjun R Warrier <notwarrier@gmail.com>
2022-07-06 12:30:07 +0530

    created window, added button

---
+++ script1.py
@@ -0,0 +1,14 @@
+from tkinter import * #going to use many functions in tkinter, 
+
+window = Tk()  #initialising window
+
+
+b1 = Button(window,text="Execute")
+
+b1.pack()
+
+
+
+
+window.mainloop() 
+
