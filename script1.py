4cd662cb2298bc45ff365886f0d40dc1e4c72fd6
Ajun R Warrier <notwarrier@gmail.com>
2022-07-26 18:26:40 +0530

    getting value from textfield

--- script1.py
+++ script1.py
@@ -3,13 +3,15 @@ from tkinter import * #going to use many functions in tkinter,
 window = Tk()  #initialising window
 
 def km_to_miles():
-    print("Km To Miles")
+    print(e1_value.get())
 
 b1 = Button(window,text="Execute", command=km_to_miles)
 
 b1.grid(row = 0,column=0)
 
-e1 = Entry(window)
+e1_value = StringVar()
+
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row = 0, column = 1)
 
 t1 = Text(window, height=1, width = 20)
